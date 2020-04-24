15ca50ec130f65ee1b1f99a2a6bfeda108fd2f56
KateZi <zinkovskayakate@gmail.com>
2020-04-24 21:07:38 +0300

    down issues

--- src/First try.py
+++ src/First try.py
@@ -18,57 +18,62 @@ def crop_frame(windowname, frame, box=None):
     return _frame, box
 
 
-path = "/Users/glebzinkovskiy/Documents/Orel_experiments/Original/Down/"
-video_name = "GX050012.mp4"
-name_tail = "_undistorted.mp4"
+def process_video(filename):
+    # if not os.path.exists(os.path.join(filename, "_undistorted.mp4):
+    #     undistort(path+video_name)
 
-# if not os.path.exists(path+video_name[:-4]+name_tail):
-#     undistort(path+video_name)
+    frame_count = 0
 
-video = cv2.VideoCapture(path + video_name) # + name_tail)
+    video = cv2.VideoCapture(filename)
 
-frame_count = 0
-box = None
+    if video.isOpened():
+        cv2.namedWindow("Frame", cv2.WINDOW_NORMAL)
+        total_count = video.get(cv2.CAP_PROP_FRAME_COUNT)
 
-screen_res = 1440, 900
+        codec = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+        # codec = cv2.VideoWriter_fourcc(*'XVID')
+        FPS = video.get(cv2.CAP_PROP_FPS)
+        _, frame = video.read()
+        # rescaled_frame = rescale_frame(frame)
+        first_frame, box = crop_frame("Frame", frame)
+        width = box[2]
+        height = box[3]
+        size = (width, height)
+        out_video = cv2.VideoWriter(str(filename[:-4]) + '_cropped.mp4', codec, FPS, size, 1)
+
+        cv2.imshow("Frame", first_frame)
+        out_video.write(first_frame)
+
+        while frame_count < total_count:
+            ret, frame = video.read()
+
+            if ret:
+                frame_count = video.get(cv2.CAP_PROP_POS_FRAMES)
+
+                # rescaled_frame = rescale_frame(frame)
+                cropped_frame, _ = crop_frame("Frame", frame, box)
+                # cv2.resizeWindow("Frame", cropped_frame.shape[1], cropped_frame.shape[0])
+                cv2.imshow("Frame", cropped_frame)
+                out_video.write(cropped_frame)
+            else:
+                print("Something's wrong: " + str(frame_count))
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+            # frame_count += 1
+
+        video.release()
+        out_video.release()
+        cv2.destroyAllWindows()
 
 
-if video.isOpened():
-    cv2.namedWindow("Frame", cv2.WINDOW_NORMAL)
-    total_count = video.get(cv2.CAP_PROP_FRAME_COUNT)
+def main():
+    path = "/Users/glebzinkovskiy/Documents/Orel_experiments/Original/Down/"
+    video_name = "GX050012.mp4"
 
-    codec = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-    # codec = cv2.VideoWriter_fourcc(*'XVID')
-    FPS = video.get(cv2.CAP_PROP_FPS)
-    _, frame = video.read()
-    rescaled_frame = rescale_frame(frame)
-    first_frame, box = crop_frame("Frame", rescaled_frame)
-    width = box[2]
-    height = box[3]
-    size = (width, height)
-    out_video = cv2.VideoWriter(str(video_name[:-4]) + '_cropped.mp4', codec, FPS, size, 1)
+    process_video(os.path.join(path, video_name))
 
-    out_video.write(first_frame)
 
-    while frame_count < 237:
-        ret, frame = video.read()
-
-        if ret:
-            frame_count = video.get(cv2.CAP_PROP_POS_FRAMES)
-
-            rescaled_frame = rescale_frame(frame)
-            cropped_frame, _ = crop_frame("Frame", rescaled_frame, box)
-            # cv2.resizeWindow("Frame", cropped_frame.shape[1], cropped_frame.shape[0])
-            # cv2.imshow("Frame", cropped_frame)
-            out_video.write(cropped_frame)
-        else:
-            print("Something's wrong: " + str(frame_count))
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-        # frame_count += 1
-
-    video.release()
-    out_video.release()
-    cv2.destroyAllWindows()
+if __name__=='__main__':
+    main()
